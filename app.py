07e4d236dd0c53b6c6af801c5313b01162461275
feduardosoares <feduardosoares.spi@gmail.com>
2026-03-24 08:41:53 -0300

    teste

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
-nome = input('Qual seu nome?')
-print ('Bem vindo ', nome)
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+        
+car_data = pd.read_csv('data/vehicles.csv') # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
+        
+if hist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+            
+    # criar um histograma
+    fig = px.histogram(car_data, x="odometer")
+    
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+        
