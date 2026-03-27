cd51c424a642e175e56a337cbb18b86c1135b6bb
feduardosoares <feduardosoares.spi@gmail.com>
2026-03-27 08:20:33 -0300

    pós solicitação de correção

--- app.py
+++ app.py
@@ -16,3 +16,14 @@ if hist_button:
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Criar gráfico de dispersão')
+
+if scatter_button: # se o botão for clicado
+    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+    
+    # criar um gráfico de dispersão
+    fig = px.scatter(car_data, x="odometer", y="price")
+    
+    # exibir o gráfico
+    st.plotly_chart(fig, use_container_width=True)
